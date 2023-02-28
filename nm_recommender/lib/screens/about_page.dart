560cc0e7ade44d7ddcc006afedccd98afd96c818
Phattharani Nook Thongsriphong <76402947+PhattharaniThongsriphong@users.noreply.github.com>
2023-03-01 04:46:23 +0700

    photootot

--- nm_recommender/lib/screens/about_page.dart
+++ nm_recommender/lib/screens/about_page.dart
@@ -9,9 +9,22 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("About Page"),
       ),
-      body: const Center(
-        child: Text("About Page"),
-      ),
+      body: AboutList(),
     );
   }
 }
+
+class AboutList extends StatefulWidget {
+  const AboutList({Key? key}) : super(key: key);
+
+  @override
+  State<AboutList> createState() => _AboutListState();
+}
+
+class _AboutListState extends State<AboutList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
